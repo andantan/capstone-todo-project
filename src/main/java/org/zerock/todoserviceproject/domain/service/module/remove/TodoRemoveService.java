81f95682926aed3d563995d3eb2eed0f63df4e10
andantan <kyubin2892@gmail.com>
2025-04-17 17:51:30 +0900

    RemoveController Update

--- src/main/java/org/zerock/todoserviceproject/domain/service/module/remove/TodoRemoveService.java
+++ src/main/java/org/zerock/todoserviceproject/domain/service/module/remove/TodoRemoveService.java
@@ -1,4 +1,13 @@
 package org.zerock.todoserviceproject.domain.service.module.remove;
 
+
+import java.util.Map;
+
+// +-+-+-+-+-+-+-+
+//     REMOVE    |
+// +-+-+-+-+-+-+-+
 public interface TodoRemoveService {
+
+    Map<String, String> requestRemove(Long tno);
+
 }
