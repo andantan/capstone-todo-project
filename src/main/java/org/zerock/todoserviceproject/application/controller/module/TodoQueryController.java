0839dfdeb08c0b09295df07860bd949fa66943ce
andantan <kyubin2892@gmail.com>
2025-04-17 18:43:19 +0900

    QueryController::queryList implemented

--- src/main/java/org/zerock/todoserviceproject/application/controller/module/TodoQueryController.java
+++ src/main/java/org/zerock/todoserviceproject/application/controller/module/TodoQueryController.java
@@ -1,14 +1,19 @@
 package org.zerock.todoserviceproject.application.controller.module;
 
 import io.swagger.v3.oas.annotations.Operation;
+import jakarta.annotation.Nullable;
+import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.data.repository.query.Param;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.zerock.todoserviceproject.application.controller.AbstractTodoController;
+import org.zerock.todoserviceproject.application.controller.util.TodoControllerUtil;
 import org.zerock.todoserviceproject.domain.service.module.query.TodoQueryService;
 
+import java.util.List;
 import java.util.Map;
 import java.util.NoSuchElementException;
 
@@ -23,16 +28,36 @@ public final class TodoQueryController extends AbstractTodoController {
 
     @Operation(
             summary = "Todo 조회",
-            description = "ResponseBodyTodoDTO 형식에 따름"
+            description = "ResponseQueryTodoDTO 형식에 따름"
     )
     @GetMapping(
-            value = "/query/{tno}"
+            value = "/query"
     )
     public Map<String, String> query(
-            @PathVariable("tno") Long tno
+            @NotNull @Param("tno") Long tno
     ) throws NoSuchElementException {
 
-        return this.todoQueryService.requestFindTodo(tno);
+        return this.todoQueryService.requestQueryTodo(tno);
+    }
+
+
+    @Operation(
+            summary = "Todo 조회 ( todo List 조회 )",
+            description = "ResponseQueryTodoDTO 형식에 따름"
+    )
+    @GetMapping(
+            value = "/query/list"
+    )
+    public List<Map<String, Object>> findAllTodoList (
+            @NotNull @Param("sort") String sort,     //  [ desc | asc ]
+            @Nullable @Param("limit") Integer limit
+    ) throws Exception {
+
+        boolean desc = TodoControllerUtil.isDescendingSorting(sort);
+
+        return (limit == null)
+                ? this.todoQueryService.requestQueryTodoList(desc)
+                : this.todoQueryService.requestQueryTodoList(desc, limit);
     }
 
 }
