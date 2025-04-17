a523f8ec2738128920bf9959af0ec2d74973252e
andantan <kyubin2892@gmail.com>
2025-04-17 12:18:26 +0900

    Initialize repository

---
+++ src/main/java/org/zerock/todoserviceproject/TodoServiceProjectApplication.java
@@ -0,0 +1,13 @@
+package org.zerock.todoserviceproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoServiceProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TodoServiceProjectApplication.class, args);
+    }
+
+}
