7cdd55168a8d5bfe479d158b625aed2af37b8080
andantan <kyubin2892@gmail.com>
2025-04-17 18:42:13 +0900

    TodoRepositoryExtension implemented - Method with condition(pagination)

--- src/main/java/org/zerock/todoserviceproject/domain/repository/TodoRepository.java
+++ src/main/java/org/zerock/todoserviceproject/domain/repository/TodoRepository.java
@@ -3,7 +3,8 @@ package org.zerock.todoserviceproject.domain.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.zerock.todoserviceproject.domain.entity.TodoEntity;
+import org.zerock.todoserviceproject.domain.repository.extension.TodoRepositoryExtension;
 
 @Repository
-public interface TodoRepository extends JpaRepository<TodoEntity, Long> {
+public interface TodoRepository extends JpaRepository<TodoEntity, Long>, TodoRepositoryExtension {
 }
--- src/main/java/org/zerock/todoserviceproject/domain/service/module/query/TodoQueryService.java
+++ src/main/java/org/zerock/todoserviceproject/domain/service/module/query/TodoQueryService.java
@@ -1,6 +1,7 @@
 package org.zerock.todoserviceproject.domain.service.module.query;
 
 
+import java.util.List;
 import java.util.Map;
 
 // +-+-+-+-+-+-+-+
@@ -8,6 +9,8 @@ import java.util.Map;
 // +-+-+-+-+-+-+-+
 public interface TodoQueryService {
 
-    Map<String, String> requestFindTodo(Long tno);
+    Map<String, String> requestQueryTodo(Long tno);
 
+    List<Map<String, Object>> requestQueryTodoList(Boolean desc);
+    List<Map<String, Object>> requestQueryTodoList(Boolean desc, Integer limit);
 }
