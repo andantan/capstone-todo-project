41d9b3077aa66b7a3e20f5ff34410cc6aec789ed
andantan <kyubin2892@gmail.com>
2025-04-19 15:03:26 +0900

    TodoRepository refactored & TodoRepositoryRemoveExtension attached

--- src/main/java/org/zerock/todoserviceproject/domain/repository/TodoRepository.java
+++ src/main/java/org/zerock/todoserviceproject/domain/repository/TodoRepository.java
@@ -3,8 +3,12 @@ package org.zerock.todoserviceproject.domain.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.zerock.todoserviceproject.domain.entity.TodoEntity;
-import org.zerock.todoserviceproject.domain.repository.extension.TodoRepositoryExtension;
+import org.zerock.todoserviceproject.domain.repository.extension.query.TodoRepositoryQueryExtension;
+import org.zerock.todoserviceproject.domain.repository.extension.remove.TodoRepositoryRemoveExtension;
 
 @Repository
-public interface TodoRepository extends JpaRepository<TodoEntity, Long>, TodoRepositoryExtension {
+public interface TodoRepository extends
+        JpaRepository<TodoEntity, Long>,
+        TodoRepositoryQueryExtension,
+        TodoRepositoryRemoveExtension {
 }
