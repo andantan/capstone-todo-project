32c1837cbf9a6a94fcc077f4e6d74bd7867edfec
andantan <kyubin2892@gmail.com>
2025-04-19 14:57:37 +0900

    archiving entity implemented - Remove query

---
+++ src/main/java/org/zerock/todoserviceproject/domain/entity/base/BaseArchiveTimeEntity.java
@@ -0,0 +1,55 @@
+package org.zerock.todoserviceproject.domain.entity.base;
+
+
+import jakarta.persistence.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.zerock.todoserviceproject.domain.entity.converter.LocalDateTimeConverter;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+@lombok.Getter
+public class BaseArchiveTimeEntity {
+
+    @CreatedDate
+    @Column(
+            name = "reg_date",
+            columnDefinition = "VARCHAR(16)",
+            updatable = false
+    )
+    @Convert(converter = LocalDateTimeConverter.class)
+    private LocalDateTime regdate;
+
+
+    @Column(
+            name = "expire_date",
+            columnDefinition = "VARCHAR(16)",
+            updatable = false
+    )
+    @Convert(converter = LocalDateTimeConverter.class)
+    private LocalDateTime expireDate;
+
+
+    @Column(
+            name = "delta",
+            nullable = false,
+            columnDefinition = "SMALLINT DEFAULT 3"
+    )
+    private Integer delta = 3;
+
+
+    @PrePersist
+    public void calculateExpireDate() {
+        if (regdate == null) {
+            regdate = LocalDateTime.now();
+        }
+        expireDate = regdate.plusDays(delta);
+    }
+
+
+    public void setDeltaDays(int newDelta) {
+        this.delta = newDelta;
+    }
+}
