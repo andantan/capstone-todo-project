6f6a10b48da0f5592cc75f55da8d0958ef1f4c96
andantan <kyubin2892@gmail.com>
2025-04-25 14:48:26 +0900

    Bug fix: TodoQueryServiceImpl::cleanUpExpiredTodos current time bug fixed

--- Server/src/main/java/org/zerock/todoserviceproject/domain/repository/todoArchive/TodoArchiveRepository.java
+++ Server/src/main/java/org/zerock/todoserviceproject/domain/repository/todoArchive/TodoArchiveRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.zerock.todoserviceproject.domain.entity.archive.TodoArchiveEntity;
 import org.zerock.todoserviceproject.domain.repository.todoArchive.extension.query.TodoArchiveRepositoryQueryExtension;
+import org.zerock.todoserviceproject.domain.repository.todoArchive.extension.remove.TodoArchiveRepositoryRemoveExtension;
 import org.zerock.todoserviceproject.domain.repository.todoArchive.extension.restore.TodoArchiveRepositoryRestoreExtension;
 
 
@@ -11,5 +12,6 @@ import org.zerock.todoserviceproject.domain.repository.todoArchive.extension.res
 public interface TodoArchiveRepository
         extends JpaRepository<TodoArchiveEntity, Long>,
         TodoArchiveRepositoryQueryExtension,
-        TodoArchiveRepositoryRestoreExtension {
+        TodoArchiveRepositoryRestoreExtension,
+        TodoArchiveRepositoryRemoveExtension {
 }
